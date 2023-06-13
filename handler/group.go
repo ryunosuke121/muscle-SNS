820d4b3e7f42055b89d4bd0ae52aca3e5e92cc70
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-06-13 23:52:20 +0900

    グループの投稿取得機能

--- handler/group.go
+++ handler/group.go
@@ -6,6 +6,9 @@ import (
 	"context"
 	"os"
 
+	"fmt"
+	"sync"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/labstack/echo/v4"
@@ -13,6 +16,11 @@ import (
 	"github.com/ryunosuke121/muscle-SNS/model"
 	"github.com/ryunosuke121/muscle-SNS/s3client"
 )
+
+type ResponseGroupPosts struct {
+	mu    sync.Mutex
+	Posts []model.Post `json:"posts"`
+}
 
 // あるグループの投稿取得
 func GetGroupPosts(c echo.Context) error {
@@ -24,14 +32,41 @@ func GetGroupPosts(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	var posts []model.Post
+	response_group_posts := ResponseGroupPosts{}
+	var wg sync.WaitGroup
 	for _, user := range *users {
-		posts = append(posts, user.Posts...)
+		for _, post := range user.Posts {
+			wg.Add(1)
+			go func(post model.Post) {
+				defer wg.Done()
+				if post.ImageUrl == "" {
+					response_group_posts.mu.Lock()
+					defer response_group_posts.mu.Unlock()
+					response_group_posts.Posts = append(response_group_posts.Posts, post)
+					return
+				}
+				// 画像を取得
+				param := &s3.GetObjectInput{
+					Bucket: aws.String(os.Getenv("BUCKET_NAME")),
+					Key:    aws.String(post.ImageUrl),
+				}
+				rq, err := s3client.PresignClient.PresignGetObject(context.Background(), param)
+				if err != nil {
+					return
+				}
+				fmt.Println(rq)
+				post.ImageUrl = rq.URL
+				response_group_posts.mu.Lock()
+				defer response_group_posts.mu.Unlock()
+				response_group_posts.Posts = append(response_group_posts.Posts, post)
+			}(post)
+		}
+		wg.Wait()
 	}
 
 	res := Response{
 		Message: "success",
-		Data:    posts,
+		Data:    response_group_posts.Posts,
 	}
 	return c.JSON(200, res)
 }
