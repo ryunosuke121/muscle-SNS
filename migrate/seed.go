6cf8162d77393ca403cdf40cb2335de372838439
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-06-13 23:40:10 +0900

    passwordを削除

--- migrate/seed.go
+++ migrate/seed.go
@@ -15,9 +15,9 @@ func seed() {
 	menus = append(menus, model.Menu{ID: 3, Name: "デッドリフト"})
 	db.Create(&menus)
 	users := []model.User{}
-	users = append(users, model.User{ID: 1, Name: "田中太郎", Email: "tekitou@example.com", Password: "tekitou", TrainingGroupID: 1})
-	users = append(users, model.User{ID: 2, Name: "佐藤花子", Email: "tekitou@example.com", Password: "tekitou", TrainingGroupID: 1})
-	users = append(users, model.User{ID: 3, Name: "山田次郎", Email: "tekitou@example.com", Password: "tekitou", TrainingGroupID: 2})
+	users = append(users, model.User{ID: 1, Name: "田中太郎", Email: "tekitou@example.com", TrainingGroupID: 1})
+	users = append(users, model.User{ID: 2, Name: "佐藤花子", Email: "tekitou@example.com", TrainingGroupID: 1})
+	users = append(users, model.User{ID: 3, Name: "山田次郎", Email: "tekitou@example.com", TrainingGroupID: 2})
 	db.Create(&users)
 	training_groups := []model.TrainingGroup{}
 	training_groups = append(training_groups, model.TrainingGroup{ID: 1, Name: "筋トレグループ1"})
