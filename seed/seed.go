bb6aa0ef80a5b7e335b7f559047c97311eb80ea4
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-06-16 00:52:50 +0900

    シードファイルを作成する

---
+++ seed/seed.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ryunosuke121/muscle-SNS/db"
+	"github.com/ryunosuke121/muscle-SNS/model"
+)
+
+func main() {
+	db := db.NewDB()
+	menus := []model.Menu{}
+	menus = append(menus, model.Menu{ID: 1, Name: "ベンチプレス"})
+	menus = append(menus, model.Menu{ID: 2, Name: "スクワット"})
+	menus = append(menus, model.Menu{ID: 3, Name: "デッドリフト"})
+	db.Create(&menus)
+	training_groups := []model.TrainingGroup{}
+	training_groups = append(training_groups, model.TrainingGroup{ID: 1, Name: "筋トレグループ"})
+	training_groups = append(training_groups, model.TrainingGroup{ID: 2, Name: "みんなで筋トレ"})
+	db.Create(&training_groups)
+	users := []model.User{}
+	users = append(users, model.User{ID: 1, Name: "田中太郎", Email: "tanaka@example.com", Password: "tekitou", TrainingGroupID: 1})
+	users = append(users, model.User{ID: 2, Name: "佐藤花子", Email: "satou@example.com", Password: "tekitou", TrainingGroupID: 1})
+	users = append(users, model.User{ID: 3, Name: "山田次郎", Email: "yamada@example.com", Password: "tekitou", TrainingGroupID: 2})
+	db.Create(&users)
+
+	fmt.Println("Successfully Seeded")
+}
